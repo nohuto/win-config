e3364d019e42c1c914c3653480e242e19ecf1be8
Nohuto <nohuto@duck.com>
2025-12-20 15:34:26 +0100

    local

--- nvidia/assets/nvcpl.ps1
+++ nvidia/assets/nvcpl.ps1
@@ -4,18 +4,18 @@ Set-Service -Name "NVDisplay.ContainerLocalSystem" -StartupType Disabled
 spsv -Name "NVDisplay.ContainerLocalSystem" -Force
 Set-Service -Name 'NvTelemetryContainer' -StartupType Disabled
 spsv -Name 'NvTelemetryContainer' -Force
-$nvroam = "$env:appdata\Noverse"
+$nvlocal = "$env:localappdata\Noverse"
 $desktop = [Environment]::GetFolderPath('Desktop')
-if (-not (Test-Path $nvroam)) {New-Item -ItemType Directory -Path $nvroam | Out-Null}
-iwr -Uri "https://github.com/nohuto/Files/releases/download/driver/nvcplui.exe" -OutFile "$nvroam\nvcplui.exe" | Out-Null
-iwr -Uri "https://github.com/nohuto/Files/releases/download/driver/Nvcpl.ico" -OutFile "$nvroam\Nvcpl.ico" | Out-Null
-$nvpsnvcpl = "$nvroam\NV-nvcpl.ps1"
+if (-not (Test-Path $nvlocal)) {New-Item -ItemType Directory -Path $nvlocal | Out-Null}
+iwr -Uri "https://github.com/nohuto/Files/releases/download/driver/nvcplui.exe" -OutFile "$nvlocal\nvcplui.exe" | Out-Null
+iwr -Uri "https://github.com/nohuto/Files/releases/download/driver/Nvcpl.ico" -OutFile "$nvlocal\Nvcpl.ico" | Out-Null
+$nvpsnvcpl = "$nvlocal\NV-nvcpl.ps1"
 $nvcplcon = @"
 `$ErrorActionPreference = "silentlycontinue"
 `$nvsrv='NVDisplay.ContainerLocalSystem','NvContainerLocalSystem'
 foreach (`$s in `$nvsrv){if(gsv `$s){Set-Service `$s -StartupType Manual
 if ((gsv `$s).Status -ne 'Running'){sasv `$s}}}
-saps -FilePath "$nvroam\nvcplui.exe" -Wait
+saps -FilePath "$nvlocal\nvcplui.exe" -Wait
 foreach (`$s in `$nvsrv){if(gsv `$s){if((gsv `$s).Status -eq 'Running'){spsv `$s -Force}
 Set-Service `$s -StartupType Disabled}}
 "@
@@ -24,6 +24,6 @@ $nvsh = New-Object -ComObject WScript.Shell
 $shortcut = $nvsh.CreateShortcut("$desktop\Nvcpl.lnk")
 $shortcut.TargetPath = "powershell.exe"
 $shortcut.Arguments = "-ExecutionPolicy Unrestricted -WindowStyle Hidden -File `"$nvpsnvcpl`""
-$shortcut.WorkingDirectory = $nvroam
-$shortcut.IconLocation = "$nvroam\Nvcpl.ico"
+$shortcut.WorkingDirectory = $nvlocal
+$shortcut.IconLocation = "$nvlocal\Nvcpl.ico"
 $shortcut.Save()
